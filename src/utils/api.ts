0d5e93f67b10cfca9e0192dd0cda5a49bfea5629
freefly0125 <simonbergman516@gmail.com>
2024-02-28 14:03:52 +0000

    feat:crypto app

---
+++ src/utils/api.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const api = () => {
+  const instance = axios.create({
+    headers: {
+      "Content-Type": "application/json",
+    },
+    baseURL: import.meta.env.VITE_BASE_URL,
+    timeout: 60000,
+  });
+  instance.interceptors.request.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
+};
+
+export default api;
